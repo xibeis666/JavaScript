4af0a63c3208f1fab95b949c7d84ec1c9d930a13
jiayinshuai <jiayinshuai.com.cn>
2019-10-24 10:30:46 +0800

    新建

---
+++ js1/index.js
@@ -0,0 +1,11 @@
+function toBig() {
+    let odiv = document.getElementById('div1');
+    odiv.style.width='200px';
+    odiv.style.height='200px';
+}
+
+function toSmall() {
+    let odiv = document.getElementById('div1');
+    odiv.style.width='100px';
+    odiv.style.height='100px';
+}
